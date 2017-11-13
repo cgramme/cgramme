f4c4b0aed387474de9c9ecd423633b5510e5a6b4
Chris Gramme <cgramme36@gmail.com>
2017-11-13 03:47:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -29,10 +29,17 @@ $(document).on({
 
 
 $(window).scroll(function () { 
-
+    var width = $(window).width();
     var scrollDistance = ($(this).scrollTop()/2);
 
-    //$('.name-under-text').text(scrollDistance);
+    if(width >= 1300){
+        $('.background-top-image').css({'top' : scrollDistance+"px"});
+        $('.first-row').css({'top' : -(($(this).scrollTop()/8)+100)+"px"}); 
+    }else{
+        $('.background-top-image').css({'top' : "0px"});
+        $('.first-row').css({'top' : '-125px'}); 
+    }
+    //$('.name-under-text').text($(window).width());
 
 
    //$('.background-top-image').css({'top' : scrollDistance+"px"}); 
@@ -45,7 +52,6 @@ $(window).scroll(function () {
 
 
    //$('.mountains').css({'top' : -($(this).scrollTop()/20)+"px"}); 
-   $('.first-row').css({'top' : -(($(this).scrollTop()/8)+100)+"px"}); 
 
    if($('body').scrollTop() > 700){
        $('.menu').addClass('opacity-in');
@@ -58,6 +64,20 @@ $(window).scroll(function () {
 
 function resizeWindow() {
     var width = $(window).width();
+
+    var mountainWidth = ($(window).width()/1350);
+
+    if(width >= 1300){
+        $('.background-top').css({'height' : '800px'});
+        $('.name-wrap').css({'top' : '190px'});
+    }else{
+        $('.background-top').css({'height' : '450px'});
+        $('.name-wrap').css({'top' : '100px'});
+    }
+
+    
+
+    $('.first-row, .second-row').css({'transform':'scale('+(mountainWidth)+')'});
    
   
     $('.child-3d').css({'height' : $('.child-3d').width()+"px"});
